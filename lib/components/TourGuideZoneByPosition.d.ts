9ca3203c92824ce981833d9d028e4f477e165e84
Sin Wai Kit <31162981+wkitsin@users.noreply.github.com>
2021-01-27 16:34:07 +0800

    lib and node modules

---
+++ lib/components/TourGuideZoneByPosition.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="react" />
+import { StyleProp, ViewStyle } from 'react-native';
+import { BorderRadiusObject, Shape } from '../types';
+interface TourGuideZoneByPositionProps {
+    zone: number;
+    isTourGuide?: boolean;
+    top?: number | string;
+    left?: number | string;
+    right?: number | string;
+    bottom?: number | string;
+    width?: number | string;
+    height?: number | string;
+    shape?: Shape;
+    borderRadiusObject?: BorderRadiusObject;
+    containerStyle?: StyleProp<ViewStyle>;
+    keepTooltipPosition?: boolean;
+    tooltipBottomOffset?: number;
+    text?: string;
+}
+export declare const TourGuideZoneByPosition: ({ isTourGuide, zone, width, height, top, left, right, bottom, shape, containerStyle, keepTooltipPosition, tooltipBottomOffset, borderRadiusObject, text, }: TourGuideZoneByPositionProps) => JSX.Element | null;
+export {};
